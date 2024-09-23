8f7f64683bbc7bf2b357e0aac29c9a8976432eac
morimorig3 <mori.nto@outlook.jp>
2024-09-23 11:28:42 +0900

    feat:Todo返却ロジックの実装

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -21,19 +22,30 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// handler
-	e.GET("/todo/:count", func(c echo.Context) error {
-		count := c.Param("count")
-		var content struct {
-			Count string `json:"count"`
-		}
-		content.Count = count
-		return c.JSON(http.StatusOK, content)
-	})
+	e.GET("/todo/:count", createTodoHandler)
 
 	// Error Handle
 	e.HTTPErrorHandler = customHTTPErrorHandler
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createTodoHandler(c echo.Context) error {
+	p := c.Param("count")
+	i, err := strconv.Atoi(p)
+
+	// 数値以外のものが渡された場合エラー
+	if err != nil {
+		c.Logger().Error(err)
+		return c.JSON(http.StatusInternalServerError, "{}")
+	}
+
+	// 100件以上エラー
+	if i > 100 {
+		return c.JSON(http.StatusInternalServerError, "{}")
+	}
+	todoList := createTodoList(i)
+	return c.JSON(http.StatusOK, todoList)
 }
 
 func customHTTPErrorHandler(err error, c echo.Context) {
@@ -45,4 +57,17 @@ func customHTTPErrorHandler(err error, c echo.Context) {
 	if err := c.JSON(code, "{}"); err != nil {
 		c.Logger().Error(err)
 	}
+}
+
+func createTodoList(cnt int) []Todo {
+	todoList := make([]Todo, cnt)
+	for i, _ := range todoList {
+		newTodo := Todo{
+			Id:          i,
+			Task:        "タスク No." + strconv.Itoa(i),
+			IsCompleted: false,
+		}
+		todoList[i] = newTodo
+	}
+	return todoList
 }
